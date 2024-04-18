bc379e92468048b5b822873a0485e4b7cc0dc351
Andre Fonseca <decobf15@gmail.com>
2024-04-18 18:04:04 -0300

    minor chage

--- src/main/java/com/abfonseca/crudspring/model/Course.java
+++ src/main/java/com/abfonseca/crudspring/model/Course.java
@@ -1,7 +1,5 @@
 package com.abfonseca.crudspring.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
