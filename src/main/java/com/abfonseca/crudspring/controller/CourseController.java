e3b0f64a25ed9ae1f30eb9794c1fb9c5aa9c3a33
Andre Fonseca <decobf15@gmail.com>
2024-04-19 18:54:09 -0300

    find by id

--- src/main/java/com/abfonseca/crudspring/controller/CourseController.java
+++ src/main/java/com/abfonseca/crudspring/controller/CourseController.java
@@ -1,9 +1,12 @@
 package com.abfonseca.crudspring.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +17,8 @@ import com.abfonseca.crudspring.repository.CourseRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
@@ -29,11 +34,17 @@ public class CourseController {
     return courseRepository.findAll();
   }
 
-  @PostMapping
-  @ResponseStatus(code = HttpStatus.CREATED)
-  public Course save(@RequestBody Course course) {
-    return courseRepository.save(course);
+  @GetMapping("/{id}")
+  public ResponseEntity<Course> findById(@PathVariable Long id) {
+      return courseRepository.findById(id)
+      .map(record -> ResponseEntity.ok().body(record))
+      .orElse(ResponseEntity.notFound().build());
   }
 
 
+  @PostMapping
+  @ResponseStatus(code = HttpStatus.CREATED)
+  public Course create(@RequestBody Course course) {
+    return courseRepository.save(course);
+  }
 }
